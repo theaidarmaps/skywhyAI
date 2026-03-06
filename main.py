31349f599d77a47735e8162aee6a2e60cd172cf3
robhul <aidarkin1010@gmail.com>
2026-03-06 13:04:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import logging
+import asyncio
+import os
+import sys
 
-from telegram import Update
-from telegram.ext import Updater
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-def main() -> None:
-    pass
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ.get('API_TOKEN')
+
+dp = Dispatcher()
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
